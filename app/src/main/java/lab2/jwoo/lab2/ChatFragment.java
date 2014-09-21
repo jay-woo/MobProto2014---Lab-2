8c9c7b40721bced321fe1cdbae31e97bbf130cd0
Jay <Jay.Woo@students.olin.edu>
2014-09-21 14:37:24 -0400

    Added a time stamp to messages

--- app/src/main/java/lab2/jwoo/lab2/ChatFragment.java
+++ app/src/main/java/lab2/jwoo/lab2/ChatFragment.java
@@ -12,6 +12,7 @@ import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 
 /**
  * Created by wooj on 9/14/14.
@@ -27,15 +28,16 @@ public class ChatFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
         final ListView myListView = (ListView) rootView.findViewById(R.id.chatbox);
         final ChatAdapter myChatAdapter = new ChatAdapter(getActivity(), this.chatItems);
+        final Calendar myCalendar = Calendar.getInstance();
         myListView.setAdapter(myChatAdapter);
 
-        Button sendButton = (Button) rootView.findViewById(R.id.sendbutton);
-        final EditText textBox = (EditText) rootView.findViewById(R.id.textbox);
+        Button sendButton = (Button) rootView.findViewById(R.id.sendButton);
+        final EditText textBox = (EditText) rootView.findViewById(R.id.textBox);
 
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                chatItems.add(new ChatItem("me", textBox.getText().toString(), "today"));
+                chatItems.add(new ChatItem("me", textBox.getText().toString(), myCalendar.getTime().toString()));
                 myChatAdapter.notifyDataSetChanged();
                 myListView.setSelection(chatItems.size() - 1);
                 textBox.setText("");
